b4fa80c9a2d3988fd897db264d1a958dd9cd10f1
STERIN PAUL <sterinpaul@gmail.com>
2024-07-26 03:10:06 +0530

    Machine test completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,16 +21,18 @@ const Home = ()=>{
     return (
         <div>
             <NavBarComp admin={false} />
-            <div className="mt-20 flex justify-center items-center">
+            <div className="mt-20 flex justify-center gap-4 items-center">
     
                     {books.length ? books.map((book)=>{
                         return(
                             <Card key={book._id} className="w-52 shadow-xl p-2 border hover:shadow-2xl">
                                 <p className="text-center capitalize text-black">{book.name}</p>
-                                <div className="overflow-hidden">
+                                <div className="overflow-hidden h-52">
                                     <img className="hover:scale-110 duration-300 w-full h-full object-cover" src={book.coverImage} alt="Cover Image" />
                                 </div>
-                                <p className="text-sm p-1 m-1 border">{book.description}</p>
+                                <div className="h-24 overflow-y-scroll border">
+                                    <p className="text-sm p-1 m-1">{book.description}</p>
+                                </div>
                                 <p className="text-center capitalize">{book.author}</p>
                                 <p className="text-center text-blue-700">{book.price}</p>
                                 <p className="text-center capitalize">{book.language}</p>
